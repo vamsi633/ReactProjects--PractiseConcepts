54ace79602884fee031cefc01ef7124361c14014
Vamsi krishna nouluri <92721694+vamsi633@users.noreply.github.com>
2023-02-04 13:26:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+import './App.css';
+
+function getRandomAnimal(){
+
+    const animals=['bird','cat','cow','dog','gator','horse'];
+
+    return animals[Math.floor(Math.random()*animals.length)];
+}
+
+
+function App(){
+    const [animals,setAnimals]=useState([]);   //starting state empty []
+
+    const handleClick=()=>{
+       setAnimals([...animals,getRandomAnimal()]);
+    };
+
+    const renderAnimals=animals.map((animal,index)=>{
+        return <AnimalShow type={animal} key={index} />
+    });
+
+    return (
+        <div className="app">
+            <button onClick={handleClick}>Add Animals</button>
+            <div className="animal-list">{renderAnimals}</div>  
+        </div>
+   )
+}
+
+export default App;
